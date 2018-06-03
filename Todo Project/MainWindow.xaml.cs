8c65afcf4f4a1862d53689dbd7d025c8c0e7fab3
patrickcsharp <32800602+patrickcsharp@users.noreply.github.com>
2018-06-03 18:07:53 +0200

    Add files via upload

---
+++ Todo Project/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Todo_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<User> users = new ObservableCollection<User>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            LB.ItemsSource = users;
+        }        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User() { Name = Textbox1.Text });
+
+        }
+        private void TodoRemove(object sender, RoutedEventArgs e)
+        {
+            if (LB.SelectedItem != null)
+                users.Remove(LB.SelectedItem as User);
+        }
+    }
+    public class User : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }    
+}
