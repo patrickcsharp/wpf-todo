ae744c2ea7c113a484eeb9dd537f5dd681481105
patrickcsharp <32800602+patrickcsharp@users.noreply.github.com>
2018-06-08 22:20:22 +0200

    Add files via upload

--- Todo Project/MainWindow.xaml.cs
+++ Todo Project/MainWindow.xaml.cs
@@ -5,11 +5,13 @@ using System.Windows;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Data.SqlClient;
 
 namespace Todo_Project
 {
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        string connectionString = "Data Source=localhost;Initial Catalog=TodoDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public Item SelectedItem { get; set; } = new Item();
         public ObservableCollection<Item> items { get; set; } = new ObservableCollection<Item>();
         private string _itemtext;
@@ -30,7 +32,18 @@ namespace Todo_Project
         {
             get
             {
-                return _AddItemCommand ?? (_AddItemCommand = new RelayCommand(() => items.Add(new Item() { Name = itemtext }), () => true));
+                return _AddItemCommand ?? (_AddItemCommand = new RelayCommand(() =>
+                {
+                    items.Add(new Item() { Name = itemtext });
+
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        string queryString = "INSERT INTO TodoItems VALUES ('" + itemtext + "')";
+                        SqlCommand command = new SqlCommand(queryString, connection);
+                        command.Connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                }, () => true));
             }
         }
         private ICommand _RemoveItemCommand;
@@ -43,6 +56,13 @@ namespace Todo_Project
                     if (SelectedItem != null)
                     {
                         items.Remove(SelectedItem as Item);
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            string queryString = "DELETE FROM TodoItems WHERE TodoItems='" + itemtext + "'";
+                            SqlCommand command = new SqlCommand(queryString, connection);
+                            command.Connection.Open();
+                            command.ExecuteNonQuery();
+                        }
                     }
                 }, () => true));
             }
@@ -56,7 +76,24 @@ namespace Todo_Project
 
         public MainWindow()
         {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                List<string> databaseitems = new List<string>();
+                string queryString = "SELECT * FROM TodoItems";
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    databaseitems.Add(String.Format("{0}", reader[0]));
+                }
+                foreach (string item in databaseitems)
+                {
+                    items.Add(new Item() { Name = item });
+                }
+            }
             InitializeComponent();
+
             this.DataContext = this;
         }
 
